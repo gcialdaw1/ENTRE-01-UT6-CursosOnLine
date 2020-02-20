6b760daafd5988c8ece1ecc13860e22bb82a8422
Garazi Cia <gcialdaw1@gmail.com>
2020-02-20 20:11:47 +0100

    hjhnv bn

--- Curso.java
+++ Curso.java
@@ -8,96 +8,106 @@ import java.time.format.DateTimeFormatter;
 public class Curso
 {
 
-	private final String ESPACIO = " ";
-	private String nombre;
-	private LocalDate fecha;
-	private Nivel nivel;
+    private final String ESPACIO = " ";
+    private String nombre;
+    private LocalDate fecha;
+    private Nivel nivel;
 
-	/**
-	 * Constructor  
-	 */
-	public Curso(String nombre, LocalDate fecha, Nivel nivel) {
+    /**
+     * Constructor  
+     */
+    public Curso(String nombre, LocalDate fecha, Nivel nivel) {
 
-		this.nombre = capitalizarNombre(nombre);
-		this.fecha = fecha;
-		this.nivel = nivel;
+        this.nombre = capitalizarNombre(nombre);
+        this.fecha = fecha;
+        this.nivel = nivel;
 
-	}
+    }
 
-	/**
-	 * Dado un nombre de curso lo devuelve capitalizado, es decir,
-	 * cada una de las palabras del nombre empiezan por mayúscula y el
-	 * resto en minúsculas. Se eliminan espacios antes y después del nombre
-	 * (asumimos entre palabras un único espacio)
-	 * 
-	 * Si nombre = " sql essential training " devuelve "Sql Essential Training"
-	 */
-	private String capitalizarNombre(String nombre) {
+    /**
+     * Dado un nombre de curso lo devuelve capitalizado, es decir,
+     * cada una de las palabras del nombre empiezan por mayúscula y el
+     * resto en minúsculas. Se eliminan espacios antes y después del nombre
+     * (asumimos entre palabras un único espacio)
+     * 
+     * Si nombre = " sql essential training " devuelve "Sql Essential Training"
+     */
+    private String capitalizarNombre(String nombre) {
+        nombre.in
+        String nnombre = nombre.substring(1, nombre.length());
+        while(!nombre.startsWith(" ") && !nombre.endsWith(" ")) {
+            if(nombre.startsWith(" ")) {
+               
+            }
+        }
+        
+        String primera = String.valueOf(nnombre.charAt(0));
+        primera = primera.toUpperCase();
+        nnombre = nnombre.replace(nnombre.charAt(0), primera.charAt(0));
+        return nnombre;
 
-		 
-		return "";
+    }
 
-	}
+    /**
+     *  accesor para el nombre
+     */
+    public String getNombre() {
 
-	/**
-	 *  accesor para el nombre
-	 */
-	public String getNombre() {
+        return this.nombre;
+    }
 
-		return this.nombre;
-	}
+    /**
+     *  accesor para el nivel
+     */
+    public Nivel getNivel() {
 
-	/**
-	 *  accesor para el nivel
-	 */
-	public Nivel getNivel() {
+        return this.nivel;
+    }
 
-		return this.nivel;
-	}
+    /**
+     *  accesor para la fecha 
+     */
+    public LocalDate getFecha() {
 
-	/**
-	 *  accesor para la fecha 
-	 */
-	public LocalDate getFecha() {
+        return fecha;
+    }
 
-		return fecha;
-	}
+    /**
+     *  accesor para la fecha formateada
+     *  Se devuelve la fecha de publicación del curso como una cadena formateada
+     *  de la forma "día nombre-mes año"  Ej. "03 diciembre 2019" 
+     */
+    public String getFechaFormateada() {
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        String strFechaActual = fecha.format(formateador);
+        return strFechaActual;
+    }
 
-	/**
-	 *  accesor para la fecha formateada
-	 *  Se devuelve la fecha de publicación del curso como una cadena formateada
-	 *  de la forma "día nombre-mes año"  Ej. "03 diciembre 2019" 
-	 */
-	public String getFechaFormateada() {
+    /**
+     *  Representación textual del curso
+     */
+    public String toString() {
 
-		return "";
-	}
+        String str = String.format("%20s: %s\n", "Nombre", this.nombre);
+        str += String.format("%20s: %s\n%20s: %s\n", "Publicado desde",
+            this.getFechaFormateada(),
+            "Nivel", this.nivel.toString());
 
-	/**
-	 *  Representación textual del curso
-	 */
-	public String toString() {
+        str += "\n";
+        return str;
+    }
 
-		String str = String.format("%20s: %s\n", "Nombre", this.nombre);
-		str += String.format("%20s: %s\n%20s: %s\n", "Publicado desde",
-		                this.getFechaFormateada(),
-		                "Nivel", this.nivel.toString());
+    /**
+     *  Probar la clase Curso
+     */
+    public static void main(String[] args) {
 
-		str += "\n";
-		return str;
-	}
-
-	/**
-	 *  Probar la clase Curso
-	 */
-	public static void main(String[] args) {
-
-		Curso curso1 = new Curso("  sql essential training ",
-		                LocalDate.of(2019, 12, 3), Nivel.PRINCIPIANTE);
-		System.out.println(curso1.toString());
-		Curso curso2 = new Curso(" wordpress PLugin development   ",
-		                LocalDate.of(2011, 11, 30), Nivel.INTERMEDIO);
-		System.out.println(curso2.toString());
-	}
+        Curso curso1 = new Curso("  sql essential training ",
+                LocalDate.of(2019, 12, 3), Nivel.PRINCIPIANTE);
+        System.out.println(curso1.toString());
+        Curso curso2 = new Curso(" wordpress PLugin development   ",
+                LocalDate.of(2011, 11, 30), Nivel.INTERMEDIO);
+        System.out.println(curso2.toString());
+    }
 
 }
